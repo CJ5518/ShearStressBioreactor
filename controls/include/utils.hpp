db1336d55f4b4feef6644a3d187bb06254f83bb1
Scribee <c1sloan6@gmail.com>
2024-01-19 10:59:53 -0800

    Add incomplete definitions for functions converting between shear stress and flow rate.

---
+++ controls/include/utils.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+class Utils {
+    public:
+        static float shearToFlow(float shear);
+        static float flowToShear(float flow);
+};
---
+++ controls/src/utils.cpp
@@ -0,0 +1,14 @@
+#include "utils.hpp"
+
+/*
+ * Returns the flow rate in ml/min required to cause the requested shear stress in mPa.
+ */
+float Utils::shearToFlow(float shear) {
+    return 0.0f; // TODO: talk to BEs about adapting the excel calc sheet
+}
+/*
+ * Returns the shear stress in mPa resulting from the provided flow rate in ml/min.
+ */
+float Utils::flowToShear(float flow) {
+    return 0.0f;
+}
