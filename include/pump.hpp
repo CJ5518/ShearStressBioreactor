bf2cccd3b3c9e8375f9960918a6f267c645a46ca
scribee <c1sloan6@gmail.com>
2024-03-26 13:57:28 -0700

    Update pump speed command limits with new testing

--- include/pump.hpp
+++ include/pump.hpp
@@ -18,16 +18,16 @@ class Pump {
         YAAJ_ModbusMaster controller;
 
         // Pump speeds in ml/min above which the precision of the pump decreases by a factor of 2
-        const int STEP_1 = 63.2;
-        const int STEP_2 = 127.5;
-        const int STEP_3 = 258;
+        const int STEP_1 = 64;
+        const int STEP_2 = 128;
+        const int STEP_3 = 256;
 
         // Register values corresponding to the above speeds
-        const int STEP_0_CMD = 0x4180; // TODO: confirm this equates to 0 ml/min
-        const int STEP_1_CMD = 0x4287;
-        const int STEP_2_CMD = 0x4309;
-        const int STEP_3_CMD = 0x438B;
-        const int STEP_4_CMD = 0x43C7; // TODO: currently 398 ml/min instead of 400, not necessarily important
+        const int STEP_0_CMD = 0x3DCC; // TODO: confirm this equates to 0 ml/min
+        const int STEP_1_CMD = 0x4280;
+        const int STEP_2_CMD = 0x4300;
+        const int STEP_3_CMD = 0x4380;
+        const int STEP_4_CMD = 0x43C8; // TODO: currently 398 ml/min instead of 400, not necessarily important
 
         // Change in pump speed in ml/min for increase of 1 in register value
         const int RATE_0 = 1 / 0.25;
