dc081f8d0acc9c5f05adcbe92c4057b6f727f5e3
scribee <c1sloan6@gmail.com>
2024-04-25 15:00:51 -0700

    Fix typing

--- include/utils.hpp
+++ include/utils.hpp
@@ -3,14 +3,14 @@
 class Utils {
     public:
         // Global static utility functions
-        double flowRate(double tau);
-        double shearStress(double tau);
-        double reynolds(double q);
+        static double flowRate(double tau);
+        static double shearStress(double tau);
+        static double reynolds(double q);
 
     private:
-        const double CHANNEL_HEIGHT = 0.0005; // m
-        const double CHANNEL_WIDTH = 0.04; // m
-        const double MU = 0.0006922; // Pa * s
-        const double RHO = 993; // kg / m^3
+        static constexpr double CHANNEL_HEIGHT = 0.0005; // m
+        static constexpr double CHANNEL_WIDTH = 0.04; // m
+        static constexpr double MU = 0.0006922; // Pa * s
+        static constexpr double RHO = 993; // kg / m^3
 };
  
